27ecb04de0c0f299a26112ae7900b424fb994f31
jingsen.zheng <jingsen.zheng@plus.ai>
2020-12-22 22:52:53 +0800

    use x_conv1 to sample initial keypoints

--- pcdet/models/backbones_3d/pfe/voxel_set_abstraction_ffps.py
+++ pcdet/models/backbones_3d/pfe/voxel_set_abstraction_ffps.py
@@ -55,12 +55,15 @@ class VoxelSetAbstractionFFps(nn.Module):
                 else:
                     mlps = SA_cfg[src_name].MLPS
                     c_in += sum([x[-1] for x in mlps])
+                    if src_name == self.model_cfg.SAMPLE_FEATURE_SOURCE:
+                        c_in += SA_cfg[src_name].MLPS[0][0] - 3
 
         self.vsa_point_feature_fusion = nn.Sequential(
             nn.Linear(c_in, self.model_cfg.NUM_OUTPUT_FEATURES, bias=False),
             nn.BatchNorm1d(self.model_cfg.NUM_OUTPUT_FEATURES),
             nn.ReLU(),
         )
+        self.FP_layer = pointnet2_stack_modules.StackPointnetFPModule(mlp=[0])
         self.num_point_features = self.model_cfg.NUM_OUTPUT_FEATURES
         self.num_point_features_before_fusion = c_in
 
@@ -85,12 +88,13 @@ class VoxelSetAbstractionFFps(nn.Module):
         if self.model_cfg.POINT_SOURCE == 'raw_points':
             src_points = batch_dict['points']
         elif self.model_cfg.POINT_SOURCE == 'voxel_centers':
-            src_points = common_utils.get_voxel_centers(
-                batch_dict['voxel_coords'][:, 1:4],
-                downsample_times=1,
-                voxel_size=self.voxel_size,
-                point_cloud_range=self.point_cloud_range
-            )
+            # src_points = common_utils.get_voxel_centers(
+            #     batch_dict['voxel_coords'][:, 1:4],
+            #     downsample_times=1,
+            #     voxel_size=self.voxel_size,
+            #     point_cloud_range=self.point_cloud_range
+            # )
+            src_points = batch_dict['voxels'][:, 0, 0:3]
             src_points = torch.cat([batch_dict['voxel_coords'][:, 0:1], src_points], dim=1)
         else:
             raise NotImplementedError
@@ -112,13 +116,18 @@ class VoxelSetAbstractionFFps(nn.Module):
             sampled_points = points[cur_idx:cur_idx+cur_cnt, :]
             sampled_points_feature = points_feature[cur_idx:cur_idx+cur_cnt, :]
             if sampled_points_feature.shape[1] < 3:
-                # features_for_fps = torch.cat([sampled_points, sampled_points_feature], dim=1).unsqueeze(dim=0).contiguous()
                 features_for_fps = sampled_points.unsqueeze(dim=0).contiguous()
             else:
+                # features_for_fps = torch.cat([sampled_points, sampled_points_feature], dim=1).unsqueeze(dim=0).contiguous()
                 features_for_fps = sampled_points_feature.unsqueeze(dim=0).contiguous()
-            cur_pt_idxs = pointnet2_stack_utils.feature_furthest_point_sample(
-                features_for_fps, num_keypoints
+            num_keypoints_fs = num_keypoints // 3 * 2
+            cur_pt_idxs_fs = pointnet2_stack_utils.feature_furthest_point_sample(
+                features_for_fps, num_keypoints_fs
             ).long()
+            cur_pt_idxs_ds = pointnet2_stack_utils.furthest_point_sample(
+                sampled_points.unsqueeze(dim=0).contiguous(), num_keypoints - num_keypoints_fs
+            ).long()
+            cur_pt_idxs = torch.cat([cur_pt_idxs_fs, cur_pt_idxs_ds], dim=1)
 
             if sampled_points.shape[0] < num_keypoints:
                 empty_num = num_keypoints - sampled_points.shape[0]
@@ -156,9 +165,23 @@ class VoxelSetAbstractionFFps(nn.Module):
 
         """
         batch_size = batch_dict['batch_size']
-        points, point_features, batch_cnt = self.get_points_for_sample(batch_dict)
-        point_features = point_features.new_zeros([point_features.shape[0], 0])
-        point_features_list = []
+        unknown, _, unknown_batch_cnt = self.get_points_for_sample(batch_dict)
+
+        cur_coords = batch_dict['multi_scale_3d_features']['x_conv1'].indices
+        known = common_utils.get_voxel_centers(
+            cur_coords[:, 1:4],
+            downsample_times=self.downsample_times_map['x_conv1'],
+            voxel_size=self.voxel_size,
+            point_cloud_range=self.point_cloud_range
+        )
+        known_batch_cnt = known.new_zeros(batch_size).int()
+        for bs_idx in range(batch_size):
+            known_batch_cnt[bs_idx] = (cur_coords[:, 0] == bs_idx).sum()
+        known_feats = batch_dict['multi_scale_3d_features']['x_conv1'].features.contiguous()
+
+        point_features = self.FP_layer(unknown, unknown_batch_cnt, known, known_batch_cnt, known_feats=known_feats)
+        points = unknown
+        batch_cnt = unknown_batch_cnt
         for num_keypoints, feature_sources in zip(self.model_cfg.NUM_KEYPOINTS, self.model_cfg.FEATURES_SOURCE):
             # sample keypoints from last keypoints
             keypoints, keypoints_features, keypoints_batch_cnt = self.get_sampled_points(
